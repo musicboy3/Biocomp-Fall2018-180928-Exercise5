b87ae6a4e3ce87c1c9538de318cd820c5aa950d5
JC Wharton <jcwhartonsoto@gmail.com>
2018-10-04 19:34:19 -0400

    Corrected errors on code and joined code for tasks 2 and 3

--- all.sh
+++ all.sh
@@ -1,3 +1,7 @@
+#1
+cat $1 | cut -d ',' -f 1,2 | sort -t"," -k1,1 -k2n,2 > gender_exp.csv
+
+#2
 #usage bash maxmin.sh FILENAME
 
 i="$(cat $1 | sort -n -r -t , -k 4 | head -n 1 | cut -d , -f 1,2,4)"
@@ -9,13 +13,7 @@ echo "The gender, yearsExperience, and wage for the lowest earner is: $j"
 k="$(cat $1 | sort -n -r -t , -k 4 | head -n 10 | grep "female" | wc -l)"
 echo "The number of females in the top ten earners in this data set is: $k"
 
-#a="$(cat $1 | cut -d , -f 3,4 | grep ^12 |tr ',' '\t' |  awk '{ total += $2 } END { print total/NR }'"
-#b="$(cat $1 | cut -d , -f 3,4 | grep ^16 |tr ',' '\t' |  awk '{ total += $2 } END { print total/NR }')"
-#c="$($b - $a | bc)"
-#echo "Those who graduated from college (16 years of school) on average make $c more than people who only graduated high school (12 years of school)"
-
-#Fixed errors in code
-#Added parenthesis in variable a, added echo command in variable c, added quote marks for variables
+#3
 a="$(cat $1 | cut -d , -f 3,4 | grep ^12 |tr ',' '\t' |  awk '{ total += $2 } END { print total/NR }')"
 b="$(cat $1 | cut -d , -f 3,4 | grep ^16 |tr ',' '\t' |  awk '{ total += $2 } END { print total/NR }')"
 c="$(echo "$b - $a" | bc)"
